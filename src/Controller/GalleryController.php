2a20d3a277b1d47f8fa650c5d99d470446bedc39
mtyc <tyc@productmemo.com>
2019-11-27 00:12:31 +0100

    adds gallery controller

---
+++ src/Controller/GalleryController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\GalleryRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class GalleryController extends AbstractController
+{
+    /**
+     * @Route("/gallery/{id}", name="gallery")
+     * @param int               $id
+     * @param GalleryRepository $galleryRepository
+     *
+     * @return Response
+     */
+    public function index(int $id, GalleryRepository $galleryRepository): Response
+    {
+        $gallery = $galleryRepository->find($id);
+
+        return $this->render('home/gallery.html.twig', [
+            'gallery' => $gallery
+        ]);
+    }
+}
